176097cf7f9f02317d1e49976d24bc21eae97e77
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-07-20 17:38:42 -0400

    6 tests pass

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,3 +1,41 @@
+class ShapeError(Exception):
+    pass
+
+def shape(vector):
+
+    return (len(vector),)
+
+def vector_add(a, b):
+    vectorsum =[]
+    if shape(a) == shape(b):
+        return [a[index] + b[index] for index, value in enumerate(a)]
+    else:
+        raise ShapeError
+        # assert shape([1]) == (1,)
+        # assert shape(m) == (2,)
+        # assert shape(v) == (3,)
+    #assert vector_add(x, y) == [1, 5, 4]
+    # for index, value in enumerate(a):
+    #     vectorsum.append(a[index] + b[index])
+    # return vectorsum
 
 
-def__init__(self,other):
+def vector_sub(a, b):
+    vectorsub =[]
+    if shape(a) != shape(b):
+        raise ShapeError
+    else:
+        return [a[index] - b[index] for index, value in enumerate(a)]
+
+            #return [a[index] - b[index] for index, value in enumerate(a)]
+def vector_sum(vectors):
+    pass
+
+
+#def compare_shapes(*args):
+    #shapes - [shape(item) for item in args]
+    #first_shape = shapes[0]
+    #some_shapes =[first_shape == shape for shape in shapes]
+    #return all(same_shapes)
+#def comare_shapes(*args):
+#    return len(set[shape(item) for item in args])) == 1
